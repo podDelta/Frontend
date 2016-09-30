716f373f8a45c12282c06f56513f1c48585e0f3f
Kennet Postigo <kennetfpostigo@gmail.com>
2016-09-29 22:49:52 -0400

    scaffolding frontend project structure

---
+++ src/containers/HomeContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from './../components/Home/Home.js';
+
+class HomeContainer extends React.Component {
+  render () {
+    return <Home />;
+  }
+}
+
+export default HomeContainer;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HomeContainer from './containers/HomeContainer.js';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <HomeContainer />,
   document.getElementById('root')
 );
